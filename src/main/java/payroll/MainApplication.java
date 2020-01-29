6e2ee06c10f59253c0b2c9465d269bf9e01c7b03
yyoel <yoelyulius@gmail.com>
2020-01-29 04:06:35 +0000

    commit

--- src/main/java/payroll/MainApplication.java
+++ src/main/java/payroll/MainApplication.java
@@ -4,16 +4,10 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.context.annotation.Bean;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-//import java.util.logging.Logger;
 
 @SpringBootApplication
 public class MainApplication {
@@ -30,7 +24,6 @@ public class MainApplication {
     @KafkaListener(topics = "transactionTopic")
     public void listen(ConsumerRecord<?, ?> cr) throws Exception{
         logger.info(cr.toString());
-        // String transactionTopic = cr.value
         this.template.send("publishDataTopic", cr.value().toString());
     }
 }
